e4913807c1a64c0b33f4b265b14fbb02a4319458
parag <paraggarg66@yahoo.in>
2019-08-08 19:19:02 -0400

    Splash Screen

--- app/src/main/java/com/example/c0752439_mad3125_midterm/UserLogin.java
+++ app/src/main/java/com/example/c0752439_mad3125_midterm/UserLogin.java
@@ -2,19 +2,16 @@ package com.example.c0752439_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class UserLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen_splash);
-        startActivity(new Intent(this, UserLogin.class));
+        setContentView(R.layout.activity_user_login);
 
 
 
-        finish();
     }
 }
