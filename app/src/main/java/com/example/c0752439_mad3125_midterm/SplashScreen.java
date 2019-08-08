7937e07238540610d4f35868fa79cbbb3f769b58
parag <paraggarg66@yahoo.in>
2019-08-08 18:49:12 -0400

    Splash Screen

---
+++ app/src/main/java/com/example/c0752439_mad3125_midterm/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.example.c0752439_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_splash);
+    }
+}
