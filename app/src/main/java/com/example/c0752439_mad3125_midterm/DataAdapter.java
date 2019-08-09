8e2cdc40b41185953af8f2e0385c03a6d24f3472
parag <paraggarg66@yahoo.in>
2019-08-09 19:30:04 -0400

    Added data Adapter Class

--- app/src/main/java/com/example/c0752439_mad3125_midterm/DataAdapter.java
+++ app/src/main/java/com/example/c0752439_mad3125_midterm/DataAdapter.java
@@ -17,6 +17,7 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.DataViewHolder
     private LayoutInflater inflater;
     Context context;
 
+    
     private onClickListener clickListener;
     private List<DataModel> dataList;
 
