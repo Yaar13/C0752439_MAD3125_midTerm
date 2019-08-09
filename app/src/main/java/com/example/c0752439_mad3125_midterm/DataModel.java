a1db3a32b5a64fe37307757d8835831b150e10fd
parag <paraggarg66@yahoo.in>
2019-08-09 19:29:41 -0400

    Added data Model Class

--- app/src/main/java/com/example/c0752439_mad3125_midterm/DataModel.java
+++ app/src/main/java/com/example/c0752439_mad3125_midterm/DataModel.java
@@ -24,6 +24,7 @@ public class DataModel {
     public String getLaunchYear() {
         return launchYear;
     }
+    
 
     public void setLaunchYear(String launchYear) {
         this.launchYear = launchYear;
